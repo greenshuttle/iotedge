bff7d1aeab1076a20397ebcd5ee8ff14df91ef58
zifeng <zifeng.1024@gmail.com>
2021-04-22 11:17:03 +0800

    Add sqlite for store device model data and stream compute status.

--- edge-core/src/main.rs
+++ edge-core/src/main.rs
@@ -1,3 +1,14 @@
+mod store;
+
+use store::rusqlite;
+
 fn main() {
-    println!("Hello, world!");
+    // 1. init store
+    let result = rusqlite::initial_store();
+    match result {
+        Ok(()) => println!(),
+        Err(err) => println!("{}", err),
+    }
+    // 2. init mqtt conn to the edge-pub
+    // 3. inti http server
 }
