5c380186590e04676a0fc5401f8df0a8a72cd58e
yangzifeng <zifeng.1024@gmail.com>
2021-05-31 17:24:43 +0800

    Processor and Message model.

--- edge-core/src/internal/process/message.rs
+++ edge-core/src/internal/process/message.rs
@@ -1,3 +1,11 @@
-struct Message {
-
+pub struct Message {
+    header: MessageHeader,
+    router: MessageRouter,
+    body: dyn Type,
 }
+
+pub trait Type {}
+
+pub struct MessageHeader {}
+
+pub struct MessageRouter {}
--- edge-core/src/internal/process/processor.rs
+++ edge-core/src/internal/process/processor.rs
@@ -1,9 +1,68 @@
-struct Processor {
+use tokio::sync::mpsc::{Sender, Receiver};
+use crate::internal::process::message::Message;
+
+pub struct Processor {
+    name: String,
+    tx: Sender<Message>,
+    rx: Receiver<Message>,
+}
+
+pub struct ProcessConfig {
 
 }
 
-trait Process {
-    fn init();
-    fn process();
-    fn start();
+pub trait Process {
+    fn init(&self);
+    fn process(&self);
+    fn start(&self);
+}
+
+pub struct Context {
+    pub processors: Vec<Box<dyn Process>>,
+}
+
+impl Context {
+    pub fn start_processors(&self) {
+        for processor in self.processors.iter() {
+            processor.start();
+        }
+    }
+}
+
+pub struct Shadow {
+    processor: Processor,
+}
+
+impl Process for Processor {
+    fn init(&self) {
+        todo!()
+    }
+
+    fn process(&self) {
+        todo!()
+    }
+
+    fn start(&self) {
+        todo!()
+    }
+}
+
+impl Process for Shadow {
+    fn init(&self) {
+        self.processor.init();
+    }
+
+    fn process(&self) {
+        self.processor.process();
+    }
+
+    fn start(&self) {
+        self.processor.start();
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test() {
+    let (tx, rx) = tokio::sync::mpsc::channel(1024);
 }
